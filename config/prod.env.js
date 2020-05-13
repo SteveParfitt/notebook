375422147f59c93884d1f8a1804b39d54b3888ec
SteveParfitt <absolutelyaperson@gmail.com>
2020-05-13 17:39:08 +0100

    Removing api keys from dev.env.js and prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,6 @@
 'use strict'
+
+
 module.exports = {
-  NODE_ENV: '"production"',
-    VUE_APP_API_KEY: '"AIzaSyBJ9GTol9kE4_-mr7cUHapEaStQhaFkkvE"',
-  VUE_APP_AUTH_DOMAIN: '"notebook-690fa.firebaseapp.com"',
-  VUE_APP_DB_URL: '"https://notebook-690fa.firebaseio.com"',
-  VUE_APP_PROJECT_ID: '"notebook-690fa"',
-  VUE_APP_STORAGE_BUCKET: '"notebook-690fa.appspot.com"',
-  VUE_APP_MESSAGING_SENDER_ID: '"335365073021"'
+  NODE_ENV: '"production"'
 }
